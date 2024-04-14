ef4c1e229870e65a33aedf0f8e66e66ba6d44e09
BVonan <bohnny14@gmail.com>
2024-04-14 02:53:33 -0700

    fixed the rotation code

--- src/amo/components/PopupRotatingAds/index.js
+++ src/amo/components/PopupRotatingAds/index.js
@@ -13,13 +13,10 @@ import MozillaPocketImage from './img/pocket.png';
 import MozillaPocketAd from './img/mozilla-pocket.svg';
 import MozillaRelayImage from './img/mozilla-relay.svg';
 
-
-
 const PopupManager = () => {
-
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isHovered, setIsHovered] = useState(false);
-  const [countdown, setCountdown] = useState(10); 
+  const [countdown, setCountdown] = useState(10);
 
   useEffect(() => {
     const timerInterval = setInterval(() => {
@@ -35,60 +32,49 @@ const PopupManager = () => {
       }
     }, 1000);
 
-    // old function 
-  // const [currentIndex, setCurrentIndex] = useState(0);
-  // const [isHovered, setIsHovered] = useState(false);
-
-  // useEffect(() => {
-  //   const timerInterval = setInterval(() => {
-  //     if (!isHovered) {
-  //       setCurrentIndex((prevIndex) => (prevIndex + 1) % 5);
-  //     }
-  //   }, 10000);
-
     return () => clearInterval(timerInterval);
   }, [isHovered]);
 
   const prevButtonHandler = () => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + 5) % 5);
+    setCountdown(10); // Reset countdown when changing ad
   };
 
   const closePopup = () => {
-    const popups = document.querySelectorAll('.popup');
-    if (popups[currentIndex]) {
-      popups[currentIndex].style.display = 'none';
-    }
+    setIsHovered(true);
   };
 
   const VPNAdText = `Surf, stream and get work done on servers in over 30 countries 
 for a secure internet connection with a new perspective.`;
-const MonitorAdText = 'See if you’ve been part of a data breach. If so, let us automatically get your private info back for you and continually monitor your identity for new leaks.';
-const PocketAdText = 'Over 10 million users rely on Pocket to discover and savor the best articles, news, stories and videos. And as a member of the Firefox family, privacy is paramount.';
-const RelayAdText = 'Protect your real email address to help control your inbox.';
-const FocusAdText = 'Your dedicated privacy browser with automatic tracking protection and ad blocking.';
+  const MonitorAdText =
+    'See if you’ve been part of a data breach. If so, let us automatically get your private info back for you and continually monitor your identity for new leaks.';
+  const PocketAdText =
+    'Over 10 million users rely on Pocket to discover and savor the best articles, news, stories and videos. And as a member of the Firefox family, privacy is paramount.';
+  const RelayAdText = 'Protect your real email address to help control your inbox.';
+  const FocusAdText = 'Your dedicated privacy browser with automatic tracking protection and ad blocking.';
 
-const progress = (10 - countdown) * 10;
-
+  const progress = ((10 - countdown) * 100) / 10;
+  
   return (
     
       <div>
-       <div id="mozilla-monitor" className="popup" style={{ display: currentIndex === 0 ? 'block' : 'none' }}
+       <div id="mozilla-VPN" className="popup" style={{ display: currentIndex === 3 ? 'block' : 'none' }}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}>
-        <div className="popup-content">
+        <div className="popup-content1">
           <button className="closebtn close-chat" onClick={closePopup} >X</button>
           <button id="prevButton1" className="toggleButton" onClick={prevButtonHandler}>Previous Ad</button>
           <div className="progress-bar-container">
         <div className="progress-bar1" style={{ width: `${progress}%` }}/></div>
-          <div className="ad-area">
-            <div className="ad-title">
-              <div className="title">
-                <img className="monitor-logo" src={MozillaVPNImage} alt="mozilla-vpn" />
+          <div className="ad-area1">
+            <div className="ad-title1">
+              <div className="title1">
+                <img className="VPN-logo" src={MozillaVPNImage} alt="mozilla-vpn" />
               </div>
             </div>
             <div id="ad-view">
-              <img className="monitor-ad-pic" src={MozillaVPNAd} alt="mozilla-vpn-ad" />
-              <div className="text">
+              <img className="VPN-ad-pic" src={MozillaVPNAd} alt="mozilla-vpn-ad" />
+              <div className="text1">
                 <h5><b>{VPNAdText}</b></h5>
               </div>
             </div>
@@ -127,8 +113,8 @@ const progress = (10 - countdown) * 10;
             </a>
           </div>
         </div>
-      </div>
-      
+      </div>      
+
       <div
         id="mozilla-pocket"
         className="popup"
@@ -136,20 +122,20 @@ const progress = (10 - countdown) * 10;
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
-        <div className="popup-content">
+        <div className="popup-content3">
           <button className="closebtn" id="close-chat3" onClick={closePopup}>X</button>
           <button id="prevButton3" className="toggleButton" onClick={prevButtonHandler}>Previous Ad</button>
           <div className="progress-bar-container">
         <div className="progress-bar3" style={{ width: `${progress}%` }}/></div>
-          <div className="ad-area">
-            <div className="ad-title">
-              <div className="title">
-                <img className="monitor-logo" src={MozillaPocketImage} alt="mozilla-pocket" />
+          <div className="ad-area3">
+            <div className="ad-title3">
+              <div className="title3">
+                <img className="pocket-logo" src={MozillaPocketImage} alt="mozilla-pocket" />
               </div>
             </div>
             <div id="ad-view">
-              <img className="monitor-ad-pic" src={MozillaPocketAd} alt="mozilla-pocket-ad" />
-              <div className="text">
+              <img className="pocket-ad-pic" src={MozillaPocketAd} alt="mozilla-pocket-ad" />
+              <div className="text3">
                 <h5><b>{PocketAdText}</b></h5>
               </div>
             </div>
@@ -164,19 +150,19 @@ const progress = (10 - countdown) * 10;
       <div
         id="mozilla-relay"
         className="popup"
-        style={{ display: currentIndex === 3 ? 'block' : 'none' }}
+        style={{ display: currentIndex === 0 ? 'block' : 'none' }}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
-        <div className="popup-content">
+        <div className="popup-content4">
           <button className="closebtn" id="close-chat4" onClick={closePopup}>X</button>
           <button id="prevButton4" className="toggleButton" onClick={prevButtonHandler}>Previous Ad</button>
           <div className="progress-bar-container">
         <div className="progress-bar4" style={{ width: `${progress}%` }}/></div>
-          <div className="ad-area">
-            <div id="ad-view">
-              <img className="monitor-ad-pic" src={MozillaRelayImage} alt="mozilla-relay-ad" />
-              <div className="text">
+          <div className="ad-area4">
+            <div id="ad-view4">
+              <img className="relay-ad-pic" src={MozillaRelayImage} alt="mozilla-relay-ad" />
+              <div className="text4">
                 <h5><b>{RelayAdText}</b></h5>
               </div>
             </div>
@@ -195,15 +181,15 @@ const progress = (10 - countdown) * 10;
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
-        <div className="popup-content">
+        <div className="popup-content5">
           <button className="closebtn" id="close-chat5"onClick={closePopup}>X</button>
           <button id="prevButton5" className="toggleButton" onClick={prevButtonHandler}>Previous Ad</button>
           <div className="progress-bar-container">
         <div className="progress-bar5" style={{ width: `${progress}%` }}/></div>
-          <div className="ad-area">
-            <div id="ad-view">
+          <div className="ad-area5">
+            <div id="ad-view5">
               <img className="monitor-ad-pic" src={MozillaFocusImage} alt="mozilla-focus-ad" />
-              <div className="text">
+              <div className="text5">
                 <h5><b>{FocusAdText}</b></h5>
               </div>
             </div>
