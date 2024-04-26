47007ec850b8b45dcd996c4e2503325a0c2d6d3a
BVonan <bohnny14@gmail.com>
2024-04-26 12:35:26 -0700

    cleaned up the code for focus ad

--- src/amo/components/GetFocusAd/index.js
+++ src/amo/components/GetFocusAd/index.js
@@ -5,38 +5,15 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { withRouter } from 'react-router-dom';
 
-import {
-  CLIENT_APP_ANDROID,
-  CLIENT_APP_FIREFOX,
-  GET_FIREFOX_BANNER_UTM_CONTENT,
-} from 'amo/constants';
-import Button from 'amo/components/Button';
-import {
-  GET_FIREFOX_BUTTON_CLICK_CATEGORY,
-  getDownloadLink,
-} from 'amo/components/GetFirefoxButton';
-import Notice from 'amo/components/Notice';
-import Link from 'amo/components/Link';
 import tracking from 'amo/tracking';
-import { isFirefox } from 'amo/utils/compatibility';
 import translate from 'amo/i18n/translate';
-import { replaceStringsWithJSX } from 'amo/i18n/utils';
-import type { UserAgentInfoType } from 'amo/reducers/api';
 import type { AppState } from 'amo/store';
-import type { I18nType } from 'amo/types/i18n';
-import type { ReactRouterLocationType } from 'amo/types/router';
 import focus from './img/focus.png';
 import focusPhone2 from './img/focusPhone2.png';
 import AdContainer from '../AdComponent/AdComponent';
 
 
 import './styles.scss';
-
-export const GET_FIREFOX_BANNER_CLICK_ACTION = 'download-firefox-banner-click';
-export const GET_FIREFOX_BANNER_DISMISS_ACTION =
-  'download-firefox-banner-dismiss';
-export const GET_FIREFOX_BANNER_DISMISS_CATEGORY =
-  'AMO Download Firefox Banner';
 
 export type Props = {||};
 
@@ -57,7 +34,7 @@ type InternalProps = {|
   location: ReactRouterLocationType,
 |};
 
-export const GetPocketAdBanner = ({
+export const GetFcousAdBanner = ({
   _tracking = tracking,
   clientApp,
   i18n,
@@ -66,68 +43,7 @@ export const GetPocketAdBanner = ({
 }: InternalProps): null | React.Node => {
   const [showOverlay, setShowOverlay] = useState(true); // State to track overlay visibility
 
-  const onButtonClick = () => {
-    _tracking.sendEvent({
-      action: GET_FIREFOX_BANNER_CLICK_ACTION,
-      category: GET_FIREFOX_BUTTON_CLICK_CATEGORY,
-    });
-  };
 
-  if (isFirefox({ userAgentInfo })) {
-    return null;
-  }
-
-  let overrideQueryParams = { utm_content: GET_FIREFOX_BANNER_UTM_CONTENT };
-  if (clientApp === CLIENT_APP_ANDROID) {
-    overrideQueryParams = {
-      ...overrideQueryParams,
-      // If there is a UTM campaign set on the visited AMO URL, pass it to
-      // the Play Store link. Otherwise, we use the fallback value.
-      utm_campaign: location.query.utm_campaign || undefined,
-    };
-  }
-
-  const replacements = [
-    [
-      'downloadLinkStart',
-      'downloadLinkEnd',
-      (text) => (
-        <Button
-          buttonType="none"
-          className="GetFirefoxBanner-button"
-          href={getDownloadLink({ clientApp, overrideQueryParams })}
-          key="GetFirefoxBanner-button"
-          onClick={onButtonClick}
-        >
-          {text}
-        </Button>
-      ),
-    ],
-  ];
-
-  if (clientApp === CLIENT_APP_ANDROID) {
-    replacements.push([
-      'linkStart',
-      'linkEnd',
-      (text) => (
-        <Link to={`/${CLIENT_APP_FIREFOX}/`} prependClientApp={false}>
-          {text}
-        </Link>
-      ),
-    ]);
-  }
-
-  const bannerContent = replaceStringsWithJSX({
-    text:
-      clientApp === CLIENT_APP_FIREFOX
-        ? i18n.gettext(`To use these add-ons, you'll need to
-            %(downloadLinkStart)s Download Firefox%(downloadLinkEnd)s`)
-        : i18n.gettext(`To use Android extensions, you'll need
-            %(downloadLinkStart)sFirefox for Android%(downloadLinkEnd)s. To
-            explore Firefox for desktop add-ons, please %(linkStart)svisit our
-            desktop site%(linkEnd)s`),
-    replacements,
-  });
 
   const dismissAdContent = () => {
     // code to dismiss or hide the HTML content
@@ -193,10 +109,10 @@ function mapStateToProps(state: AppState): PropsFromState {
   };
 }
 
-const GetPocketAd: React.ComponentType<Props> = compose(
+const GetFcousAd: React.ComponentType<Props> = compose(
   withRouter,
   connect(mapStateToProps),
   translate(),
-)(GetPocketAdBanner);
+)(GetFcousAdBanner);
 
-export default GetPocketAd;
+export default GetFcousAd;
