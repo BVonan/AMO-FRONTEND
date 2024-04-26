df8880ff788497fd5081e709e8e7b883b476e002
elkevino6 <elkevino6@gmail.com>
2024-04-25 22:38:39 -0700

    started from scratch, moved functions from popuprotating ads to its own file, commenting out on old file for now

---
+++ src/amo/components/MozillaVPNPopup/styles.scss
@@ -0,0 +1,4 @@
+import React, { useEffect, useState } from 'react';
+import { prevButtonHandler, closePopup } from './PopupFunctions';
+import MozillaVPNImage from './img/mozilla-vpn-brand.svg';
+import MozillaVPNAd from './img/mozilla-vpn.svg';
---
+++ src/amo/components/PopupFunctions/PopupFunctions.js
@@ -0,0 +1,11 @@
+export const prevButtonHandler = (currentIndex, setCurrentIndex, setCountdown) => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + 5) % 5);
+    setCountdown(10);
+  };
+  
+  export const closePopup = (currentIndex) => {
+    const popups = document.querySelectorAll('.popup');
+    if (popups[currentIndex]) {
+      popups[currentIndex].style.display = 'none';
+    }
+  };
